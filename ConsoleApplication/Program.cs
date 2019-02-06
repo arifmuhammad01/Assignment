56a41fbf5d09ca9c3fc2fcb09a2b4f4d2d91c385
Arif <arifbinhamid01@gmail.com>
2019-02-07 00:49:25 +0600

    Second Upload

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Your Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your Date of Birth:");
+            string DateOfBirth = Console.ReadLine();
+            Console.WriteLine("Enter your Age");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t\t\t::::::Your Credentials::::::\n");
+            Console.WriteLine("\tName:" + " " + name);
+            Console.WriteLine("\tAddress:" + " " + address);
+            Console.WriteLine("\tDate of Birth:" + " " + DateOfBirth);
+            Console.WriteLine("\tAge:" + " " + age);
+            Console.ReadKey();
+        }
+    }
+}
